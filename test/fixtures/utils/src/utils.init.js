9c53b0cbbfdd632f810a4957e38218fc9758aa42
Gaetan SENN <gaetan.senn@gmail.com>
2017-10-22 00:11:54 +0200

    Refactoring to new mono version (6.0.0)

--- test/fixtures/utils/src/utils.init.js
+++ test/fixtures/utils/src/utils.init.js
@@ -1,21 +1,21 @@
 const { client } = require('../../../../')
 
-module.exports = async function ({ conf }) {
+module.exports = async function ({ conf, log }) {
 	const index = conf.indexName = 'mono-elasticsearch'
 	const type = conf.type = 'user'
 
-	this.log.info(`Deleting ${index} index`)
+	log.info(`Deleting ${index} index`)
 	await client.indices.delete({
 		index
 	})
 
-	this.log.info(`Creating ${index} index`)
+	log.info(`Creating ${index} index`)
 	await client.indices.create({
 		index
 	})
 
 	const nbDocs = 47
-	this.log.info(`Inserting ${nbDocs} documents into index ${index}...`)
+	log.info(`Inserting ${nbDocs} documents into index ${index}...`)
 
 	for (let i = 1; i <= 47; i++) {
 		await client.index({
